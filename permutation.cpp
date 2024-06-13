c80aa936774c265a4d5a0ce8a5e7746156baceb9
Hifzu04 <hifzurbtech@gmail.com>
2024-06-13 17:50:01 +0530

    accepted everycases except n=4 and n=1

---
+++ permutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    if (n<=4){
+        cout<<"NO SOLUTION";
+        return 0;
+    }
+    for (long long i = n; i >= 1; i--)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    for (long long i = n; i >= 1; i--)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << " ";
+        }
+    }
+}
