415fc8864965ad3e702d97de2ee86601412a0eda
Hifzu04 <hifzurbtech@gmail.com>
2024-06-18 11:45:09 +0530

    good problem

--- numberSpiral.cpp
+++ numberSpiral.cpp
@@ -1,5 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
+
+inline long long diagonalElement(long long x)
+{
+    return x * x - (x - 1);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long t, row, col;
+    cin >> t;
+    for (long long i = 0; i < t; i++)
+    {
+        cin >> row >> col;
+        if (row == col)
+        {
+            cout << diagonalElement(row) << endl;
+        }
+        else if (row > col)
+        {
+            if (row % 2 == 0)
+            {
+                cout << diagonalElement(row) + (row - col) << endl;
+            }
+            else
+            {
+                cout << diagonalElement(row) - (row - col) << endl;
+            }
+        }
+        else
+        {
+            if (col % 2 == 0)
+            {
+                cout << diagonalElement(col) - (col - row)<<endl;
+            }
+            else
+            {
+                cout << diagonalElement(col) + (col - row)<<endl;
+            }
+        }
+    }
+
+    return 0;
 }
