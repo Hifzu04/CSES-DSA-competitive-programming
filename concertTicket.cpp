d8b0d3de08c0acfbd14b8a9190969c03b61780a8
Hifzu04 <hifzurbtech@gmail.com>
2024-07-18 14:12:49 +0530

    failed some test cases

---
+++ concertTicket.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int  main()
+{
+    long long n, m;
+    cin >> n;
+    cin >> m;
+    long long arr1[n];
+    long long arr2[m];
+    for (long long i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+    for (long long i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    sort(arr1, arr1+n);
+    sort(arr2 , arr2+m);
+    long long p = n-1;
+    long long q= m-1;
+
+    while(p>=0){
+        if(arr2[q] >= arr1[p]){
+            cout<<arr1[p]<<endl;
+            q--;
+           
+        }
+        p--;
+    }
+    
+    for(long long i=0 ;i<=q ; i++){
+        cout<<-1<<endl;
+    }
+
+}
