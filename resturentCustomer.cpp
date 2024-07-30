9886601e825c168016bcc3d72e839d7aa546a70a
Hifzu04 <hifzurbtech@gmail.com>
2024-07-30 18:08:35 +0530

    init

---
+++ resturentCustomer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    int numberCustomer ;
+    cin>>numberCustomer;
+    int arr;
+    int dept;
+    map<int,int>mpp;
+    while(numberCustomer--){
+        cin>>arr>>dept;
+        mpp.emplace(arr, dept);
+    }
+    int count =1 ; 
+
+      for (auto it :mpp){
+        if(it.first  > )
+      }
+
+    
+
+
+}
