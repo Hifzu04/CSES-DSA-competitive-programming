64c0eba812cc6833802d064213bd67c587c97895
Hifzu04 <hifzurbtech@gmail.com>
2024-08-08 23:48:52 +0530

    can be solved using sorted pair and then compare if the next movie start after/just after the end of curr movie

---
+++ movieFestival.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    map<int, int> mpp;
+
+    ll n;
+    cin >> n;
+    while (n--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        mpp[a] = b;
+    }
+  
+
+  
+}
