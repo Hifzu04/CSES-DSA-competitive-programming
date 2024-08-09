0e9ac080df1113cb943adc2e632f777709a4b7af
Hifzu04 <hifzurbtech@gmail.com>
2024-08-09 23:54:10 +0530

    pgs

--- movieFestival.cpp
+++ movieFestival.cpp
@@ -2,9 +2,22 @@
 #define ll long long
 using namespace std;
 
+
+int solve(int n,vector<pair<int,int> >&vec ){
+       
+}
+
 int main()
 
+
+
 {
-    vector<pair<int,int>>vec;
-    for()
+   
+    ll n ;
+    cin>>n;
+    vector<pair<int,int>>vec(n);
+    for(int i=0; i<n; i++){
+        cin>>vec[i].first >>vec[i].second;
+    }
+    cout<<solve(n , vec)<<endl;
 }
