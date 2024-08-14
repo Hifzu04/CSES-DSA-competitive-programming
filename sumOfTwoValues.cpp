b5e802a632468de94fcc9ba4dc0d46b58e5eb7aa
Hifzu04 <hifzurbtech@gmail.com>
2024-08-14 21:46:01 +0530

    how to optimise it

--- sumOfTwoValues.cpp
+++ sumOfTwoValues.cpp
@@ -2,6 +2,19 @@
 #include <algorithm>
 using namespace std;
 
+int binarySearch(long long key, long long arr[], long long n)
+{
+    long long s = 0;
+    sort(arr.begin , arr.end , )
+    while(s<n){
+        if(key==arr[s]){
+            return arr[s] ;
+        }
+        else if (key<arr[s]){
+            
+        }
+    }
+}
 int main()
 {
     long long n, x;
@@ -15,13 +28,16 @@ int main()
     for (long long i = 0; i < n; i++)
     {
         long long key = x - arr[i];
-        long long *it = find(arr, arr + n, key);
-        //cout<<*it<<endl;
-        if (it != arr+ n &&  i != (it-arr))
-        {
-            cout <<i+1<<" "<< (it-arr)+1;
-            return 0;
-        }
+
+        int it = binarySearch(key, arr, n);
+
+        // long long *it = find(arr, arr + n, key);
+        //  // //cout<<*it<<endl;
+        //  // if (it != arr+ n &&  i != (it-arr))
+        //  // {
+        //  //     cout <<i+1<<" "<< (it-arr)+1;
+        //  //     return 0;
+        //  // }
     }
     cout << "IMPOSSIBLE";
     return 0;
