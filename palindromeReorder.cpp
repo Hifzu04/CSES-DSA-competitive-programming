89a28df77fb0acb16141c45a188331195a837274
Hifzu04 <hifzurbtech@gmail.com>
2024-08-16 23:10:19 +0530

    fails sometest cases

---
+++ palindromeReorder.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str;
+    cin >> str;
+
+    // cout<<string(3 , 'a');
+    unordered_map<char, ll> mpp;
+    for (char c : str)
+    {
+        mpp[c]++;
+    }
+    ll count = 0;
+    char centerChar = 'p';
+    for (auto it : mpp)
+    {
+        if (it.second % 2 != 0)
+        {
+            count++;
+            centerChar = it.first;
+           
+            if (count > 1)
+            {
+                cout << "NO SOLUTION";
+                return 0;
+            }
+        }
+    }
+    string firsthalf = "";
+    for (auto it : mpp)
+    {
+        firsthalf += string(it.second / 2, it.first);
+    }
+     string secondhalf = "";
+    for(ll i =firsthalf.length()-1 ;i>= 0 ; i--){
+       secondhalf +=firsthalf[i];
+    }
+
+   
+   
+   
+    cout << firsthalf+centerChar+secondhalf;
+
+    return 0;
+}
